3aa946a280b84b6757f058ed5e31ec4d01342c0b
Pavel Mikulas <pavel.mikulas@softvision.ai>
2020-11-18 15:37:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 setup(name='best',
       description='Bayesian estimation supersedes the t test',
-      author='Andrew Straw',
-      author_email='strawman@astraw.com',
-      url='https://github.com/strawlab/best',
+      author='Pavel Mikulas',
+      author_email='mikulaspa@gmail.com',
+      url='https://github.com/fassty/best',
       version='1.0',
       packages=['best'],
       license='MIT',
