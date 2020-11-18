40f12c3bd8425ac9e1a1690792cd17defcaba133
Pavel Mikulas <pavel.mikulas@softvision.ai>
2020-11-18 15:39:15 +0100

    Remove main from model

--- best/model.py
+++ best/model.py
@@ -39,24 +39,3 @@ class Model:
             trace = pm.sample(it)
 
         return posterior, trace
-
-if __name__ == '__main__':
-    drug = (101, 100, 102, 104, 102, 97, 105, 105, 98, 101, 100, 123, 105, 103, 100, 95, 102, 106,
-            109, 102, 82, 102, 100, 102, 102, 101, 102, 102, 103, 103, 97, 97, 103, 101, 97, 104,
-            96, 103, 124, 101, 101, 100, 101, 101, 104, 100, 101)
-    placebo = (99, 101, 100, 101, 102, 100, 97, 101, 104, 101, 102, 102, 100, 105, 88, 101, 100,
-               104, 100, 100, 100, 101, 102, 103, 97, 101, 101, 100, 101, 99, 101, 100, 100,
-               101, 100, 99, 101, 100, 102, 99, 100, 99, 100, 100, 100, 100, 100)
-
-    drug = np.array(drug)
-    placebo = np.array(placebo)
-    drug.sort()
-    placebo.sort()
-
-    model = Model(drug, placebo)
-    trace = model.sample(rope_width=0.5, plot_posterior=True)
-
-    mode, p_above_0, p_rope = get_mean_metrics(trace, rope_width=0.5)
-
-    if p_above_0 > 91 and p_rope < 15:
-        print('The drug really works, who would have thought')
