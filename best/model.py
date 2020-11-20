d462f3ff643f43c2bc2b927714e47d2b1b5e0e95
Pavel Mikulas <pavel.mikulas@softvision.ai>
2020-11-20 09:42:03 +0100

    Only use one chain

--- best/model.py
+++ best/model.py
@@ -8,7 +8,7 @@ class Model:
     NORMALITY_THRESHOLD = 29  # since 30 the distribution is considered normal
 
     def __init__(self, group_1: np.ndarray, group_2: np.ndarray):
-        self._diff = (group_1 - group_2) + np.finfo(np.float32).eps
+        self._diff = group_1 - group_2
         self._diff_std = np.std(self._diff).item()
         self._diff_mean = np.mean(self._diff).item()
         self._mu = self._diff_mean
@@ -30,6 +30,6 @@ class Model:
             effect_size = pm.Deterministic('Effect size', mu / sigma)
 
         with posterior:
-            trace = pm.sample(it, chains=1)
+            trace = pm.sample(it, chains=1, progressbar=False)
 
         return posterior, trace
