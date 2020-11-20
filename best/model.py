3bb1be1cdf021596ea64107063789c940fdde9b3
Pavel Mikulas <pavel.mikulas@softvision.ai>
2020-11-20 14:58:07 +0100

    Allow passing of custom models

--- best/model.py
+++ best/model.py
@@ -5,34 +5,57 @@ import logging
 logger = logging.getLogger('pymc3')
 logger.setLevel(logging.ERROR)
 
+
 class Model:
     PRECISION_SCALING = 1e-6
     SIGMA_SCALING = 1e3
     NORMALITY_THRESHOLD = 29  # since 30 the distribution is considered normal
 
-    def __init__(self, group_1: np.ndarray, group_2: np.ndarray):
-        self._diff = group_1 - group_2
-        self._diff_std = np.std(self._diff).item()
-        self._diff_mean = np.mean(self._diff).item()
-        self._mu = self._diff_mean
-        self._tau = self.PRECISION_SCALING / np.power(self._diff_std, 2)
-        self._sigma_low = self._diff_std / self.SIGMA_SCALING
-        self._sigma_high = self._diff_std * self.SIGMA_SCALING
+    def __init__(self, model: pm.Model):
+        super().__init__()
 
-    def sample(self, it: int = 110000):
-        with pm.Model() as posterior:
-            mu = pm.Normal(name='prior_mu', mu=self._mu, tau=self._tau)
-            sigma = pm.Uniform(name='prior_sigma', lower=self._sigma_low, upper=self._sigma_high)
-            nu = pm.Exponential(name='Normality', lam=1 / self.NORMALITY_THRESHOLD) + 1
+        self.model = model
+
+    @classmethod
+    def from_custom_model(cls, model: pm.Model):
+        """
+        As of now this method assumes that the posterior distribution has a name `posterior_dist` and
+        that it contains 4 parameters: Normality, Mean, Std. dev, Effect size.
+
+        Passing a different model is fine for sampling but the plotting will fail
+        """
+        return cls(model)
+
+    @classmethod
+    def from_two_groups(cls, group_1: np.ndarray, group_2: np.ndarray):
+        diff = group_1 - group_2
+        return Model.from_one_group(diff)
+
+    @classmethod
+    def from_one_group(cls, diff: np.ndarray):
+        diff_std = np.std(diff).item()
+        diff_mean = np.mean(diff).item()
+        mu = diff_mean
+        tau = cls.PRECISION_SCALING / np.power(diff_std, 2)
+        sigma_low = diff_std / cls.SIGMA_SCALING
+        sigma_high = diff_std * cls.SIGMA_SCALING
+
+        with pm.Model() as model:
+            mu = pm.Normal(name='prior_mu', mu=mu, tau=tau)
+            sigma = pm.Uniform(name='prior_sigma', lower=sigma_low, upper=sigma_high)
+            nu = pm.Exponential(name='Normality', lam=1 / cls.NORMALITY_THRESHOLD) + 1
             lam = sigma ** -2
 
-            r = pm.StudentT('posterior_dist', mu=mu, lam=lam, nu=nu, observed=self._diff)
+            r = pm.StudentT('posterior_dist', mu=mu, lam=lam, nu=nu, observed=diff)
 
             mean = pm.Deterministic('Mean', mu)
             std = pm.Deterministic('Std. dev', sigma)
             effect_size = pm.Deterministic('Effect size', mu / sigma)
 
-        with posterior:
+        return cls(model)
+
+    def sample(self, it: int = 110000):
+        with self.model:
             trace = pm.sample(it, chains=1, progressbar=False)
 
-        return posterior, trace
+        return self.model, trace
